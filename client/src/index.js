9e0226ecc1b4fe885a8e2744b18508df4c0121b6
John <john.g@roofandfloor.com>
2018-08-11 23:54:23 +0530

    webpack set up done basic client ready

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/app';
+const Index = () => {
+  return <div>Hello React!</div>;
+};
+
+ReactDOM.render(<App/>, document.getElementById("index"));
