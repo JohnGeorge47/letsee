3fc465b04c556826a06e77143f3cb15e20efd057
John <john.g@roofandfloor.com>
2018-08-13 21:20:55 +0530

    added contact form with material ui

--- client/src/components/contact/contact.js
+++ client/src/components/contact/contact.js
@@ -1,11 +1,58 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-
+import Grid from "@material-ui/core/Grid";
+import Input from "@material-ui/core/Input";
+import "./contact.css";
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", lastName: "", email: "" };
+  }
+  handleChange(name) {
+    this.setState({
+      name
+    });
+  }
+  handleChangeLast(lastName) {
+    this.setState({
+      lastName
+    });
+  }
   render() {
+    console.log(this.state);
     return (
-      <div className="contactForm">
-        <h1>Drop a message and we'll get back to you</h1>{" "}
+      <div>
+        <h1 className="contactForm">Drop a message and well get back to you</h1>
+        <form>
+          <Grid container spacing={8}>
+            <Grid item xs={6}>
+              <Input
+                placeholder="Your First Name "
+                onChange={e => {
+                  this.handleChange(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Input
+                placeholder="Your Last Name"
+                onChange={e => {
+                  this.handleChangeLast(e.target.value);
+                }}
+              />
+            </Grid>
+          </Grid>
+          <Grid container spacing={8}>
+            <Input
+              placeholder="Your Email Id"
+              onChange={e => {
+                this.setState({
+                  email: e.target.value
+                });
+              }}
+            />
+          </Grid>
+        </form>
       </div>
     );
   }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,23 +21,10 @@ module.exports = {
           loader: "babel-loader"
         }
       },
+
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
-            }
-          }
-        ]
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
