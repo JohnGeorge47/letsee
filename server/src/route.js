88f81ba9c6d6602be3a21abf0ea9e0369e6c9f2d
John George <johngeorge.ctprice@gmail.com>
2018-09-03 12:38:31 +0530

    insert user into db with hashing

--- server/src/route.js
+++ server/src/route.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import testController from './Controllers/testController';
 import playerNameController from './Controllers/playerNameController';
+import userController from './Controllers/userController';
+import smsController from './Controllers/smsController';
 const router = express.Router();
 router.get('/test', testController.get);
 router.get('/playernames', playerNameController.get);
+router.post('/user', userController.post);
+router.post('/sms', smsController.post);
 export default router;
